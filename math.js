18bdffbe2d64156f45b49d58bcb382982146dc37
Madhaneey <143588763+Madhan-15@users.noreply.github.com>
2024-08-16 11:48:21 +0530

    Delete math.js

--- math.js
+++
@@ -1,9 +0,0 @@
-//79 vadhu line export ah vera mariuh panlaam....
-exports.ad=(a,b) => a+b;
-// const add =(a,b) => a+b;
-// const sub =(a,b) => a-b;
-// const multi =(a,b) => a*b;
-// const divis =(a,b) => a/b;
-
-//idha export pana dha use pana mudiuh adhuku .... module.export aprm destructure panum // aprm server.js la import panum 
-//module.exports={add,sub,multi,divis} 
